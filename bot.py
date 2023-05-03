30aee70a0c5abd0627e3140a8c181390635ee39a
TOBOLL <j.tob@wp.pl>
2023-05-03 18:00:50 +0100

    clear command added

--- bot.py
+++ bot.py
@@ -1,5 +1,12 @@
 import discord
+from discord.ext import commands
+
 import response
+
+token = open('token.txt', 'r').readline()
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(command_prefix='!', intents=intents)
 
 
 async def send_message(message, user_message):
@@ -11,17 +18,20 @@ async def send_message(message, user_message):
 
 
 def run_discord_bot():
-    token = open('token.txt', 'r').readline()
-    intents = discord.Intents.default()
-    intents.message_content = True
-    client = discord.Client(intents=intents)
-
     @client.event
     async def on_message(message):
+        message_content_string = str(message.content)
         if message.author == client.user or str(message.channel) != "bot":
             return
 
-        await send_message(message, str(message.content))
+        if message_content_string[0] != '!':
+            await send_message(message, message_content_string)
+        else:
+            await client.process_commands(message)
+
+    @client.command(name='clear', help='this command will clear 15 msgs')
+    async def clear(ctx):
+        await ctx.channel.purge(limit=25)
 
     @client.event
     async def on_ready():
